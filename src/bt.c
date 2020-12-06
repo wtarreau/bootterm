9c0113212d4628c273a6f0e71a336a002e62c3ab
Willy Tarreau <w@1wt.eu>
2020-12-06 21:17:44 +0100

    src: open the port and configure it
    This implements support for posix settings as well as the new linux one
    which supports custom speeds. This one will possibly not always build as
    it requires inclusion of asm/termbits.h so we have an option NO_TCGETS2
    to disable it.

--- src/bt.c
+++ src/bt.c
@@ -34,6 +34,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#if defined(__linux__) && !defined(NO_TCGETS2)
+#include <sys/ioctl.h>
+#include <asm/termbits.h>
+#else
+#include <termios.h>
+#endif
 #include <time.h>
 #include <unistd.h>
 
@@ -43,8 +49,15 @@
 #define VERSION "0.0.1"
 #endif
 
+#ifndef CRTSCTS
+#define CRTSCTS 0
+#endif
+
 const char version_str[] =
 	"BootTerm " VERSION " -- the terminal written for users by its users\n"
+#ifdef TCGETS2
+	"Built with support for custom baud rates (TCGETS2).\n"
+#endif
 	"Copyright (C) 2020 Willy Tarreau <w@1wt.eu>\n"
 	"This is free software: you are encouraged to improve and redistribute it.\n"
 	"There is no warranty of any kind, see the source for license details.\n";
@@ -329,6 +342,233 @@ void list_ports(int portspec)
 	putchar('\n');
 }
 
+/* Try to open this port (prepending /dev if the path doesn't start with '/').
+ * The FD is returned on success, otherwise -1 with errno set appropriately.
+ */
+int open_port(const char *port)
+{
+	char ftmp[PATH_MAX];
+	int fd;
+
+	if (snprintf(ftmp, sizeof(ftmp),
+		     (*port == '/') ? "%s" : "/dev/%s",
+		     port) > sizeof(ftmp)) {
+		errno = ENAMETOOLONG;
+		return -1;
+	}
+
+	fd = open(ftmp, O_RDWR | O_NONBLOCK | O_NOCTTY);
+	return fd;
+}
+
+#ifdef TCSETS2 // since linux 2.6.20 (commit edc6afc54)
+/* try to set the baud rate and mode on the port corresponding to the fd. The
+ * baud rate remains unchanged if <baud> is zero. We have two APIs, one using
+ * TCSETS and one using TCSETS2, which allows non-standard baud rates. On
+ * failure -1 is returned with errno set, otherwise zero is returned. If a
+ * baud rate couldn't be found, -1 is returned and errno set.
+ */
+int set_port(int fd, int baud)
+{
+	struct termios2 tio;
+
+	ioctl(fd, TCGETS2, &tio);
+	/* c_cflag serves to set both directions at once this way (CBAUD is a
+	 * mask for all the Bxxxx speed bits) (see termbits.h):
+	 *    (Bxxxx & CBAUD) << IBSHIFT for the input speed
+	 *    (Bxxxx & CBAUD) for the output speed.
+	 * A special baud rate value, BOTHER, indicates that the speed is to be
+	 * taken from the c_ispeed and c_ospeed fields instead.
+	 */
+	tio.c_cflag  = (tio.c_cflag & ~CSIZE) | CS8; // 8-bit
+	tio.c_cflag  = (tio.c_cflag & ~CSTOPB);      // 1-stop
+	tio.c_cflag &= ~(PARENB | PARODD);           // no parity
+	tio.c_cflag &= ~HUPCL;                       // no DTR/RTS down
+	tio.c_cflag &= ~CRTSCTS;                     // no flow control
+	tio.c_cflag |= CREAD;                        // enable rx
+	tio.c_cflag |= CLOCAL;                       // local echo
+
+	if (baud) {
+		tio.c_cflag &= ~(CBAUD | (CBAUD << IBSHIFT));
+		tio.c_cflag |= BOTHER | (BOTHER << IBSHIFT);
+		tio.c_ospeed = tio.c_ispeed = baud;
+	}
+
+	return ioctl(fd, TCSETS2, &tio);
+}
+
+#else /* TCSETS2 not defined */
+
+/* returns a combination of Bxxxx flags to set on termios c_cflag depending on
+ * the baud rate values supported on the platform. Only exact values match. If
+ * no value is found, (tcflag_t)~0 is returned, assuming it will never match an
+ * existing value on any platform.
+ */
+tcflag_t baud_encode(int baud)
+{
+	struct {
+		int b;      // baud rate
+		tcflag_t f; // corresponding flag
+	} speeds[] = {
+#ifdef B50
+		{ .b = 50, .f = B50 },
+#endif
+#ifdef B75
+		{ .b = 75, .f = B75 },
+#endif
+#ifdef B110
+		{ .b = 110, .f = B110 },
+#endif
+#ifdef B134
+		{ .b = 134, .f = B134 },
+#endif
+#ifdef B150
+		{ .b = 150, .f = B150 },
+#endif
+#ifdef B200
+		{ .b = 200, .f = B200 },
+#endif
+#ifdef B300
+		{ .b = 300, .f = B300 },
+#endif
+#ifdef B600
+		{ .b = 600, .f = B600 },
+#endif
+#ifdef B1200
+		{ .b = 1200, .f = B1200 },
+#endif
+#ifdef B1800
+		{ .b = 1800, .f = B1800 },
+#endif
+#ifdef B2400
+		{ .b = 2400, .f = B2400 },
+#endif
+#ifdef B4800
+		{ .b = 4800, .f = B4800 },
+#endif
+#ifdef B9600
+		{ .b = 9600, .f = B9600 },
+#endif
+#ifdef B19200
+		{ .b = 19200, .f = B19200 },
+#endif
+#ifdef B38400
+		{ .b = 38400, .f = B38400 },
+#endif
+#ifdef B57600
+		{ .b = 57600, .f = B57600 },
+#endif
+#ifdef B76800
+		{ .b = 76800, .f = B76800 },
+#endif
+#ifdef B115200
+		{ .b = 115200, .f = B115200 },
+#endif
+#ifdef B153600
+		{ .b = 153600, .f = B153600 },
+#endif
+#ifdef B230400
+		{ .b = 230400, .f = B230400 },
+#endif
+#ifdef B307200
+		{ .b = 307200, .f = B307200 },
+#endif
+#ifdef B460800
+		{ .b = 460800, .f = B460800 },
+#endif
+#ifdef B500000
+		{ .b = 500000, .f = B500000 },
+#endif
+#ifdef B576000
+		{ .b = 576000, .f = B576000 },
+#endif
+#ifdef B614400
+		{ .b = 614400, .f = B614400 },
+#endif
+#ifdef B921600
+		{ .b = 921600, .f = B921600 },
+#endif
+#ifdef B1000000
+		{ .b = 1000000, .f = B1000000 },
+#endif
+#ifdef B1152000
+		{ .b = 1152000, .f = B1152000 },
+#endif
+#ifdef B1500000
+		{ .b = 1500000, .f = B1500000 },
+#endif
+#ifdef B2000000
+		{ .b = 2000000, .f = B2000000 },
+#endif
+#ifdef B2500000
+		{ .b = 2500000, .f = B2500000 },
+#endif
+#ifdef B3000000
+		{ .b = 3000000, .f = B3000000 },
+#endif
+#ifdef B3500000
+		{ .b = 3500000, .f = B3500000 },
+#endif
+#ifdef B4000000
+		{ .b = 4000000, .f = B4000000 },
+#endif
+		{ .b = 0, .f = ~0 } // end
+	};
+	int idx;
+
+	for (idx = 0; speeds[idx].b; idx++) {
+		if (speeds[idx].b == baud)
+			return speeds[idx].f;
+	}
+	return speeds[idx].f;
+}
+
+/* try to set the baud rate and mode on the port corresponding to the fd. The
+ * baud rate remains unchanged if <baud> is zero. We have two APIs, one using
+ * TCSETS and one using TCSETS2, which allows non-standard baud rates. On
+ * failure -1 is returned with errno set, otherwise zero is returned. If a
+ * baud rate couldn't be found, -1 is returned and errno set.
+ */
+int set_port(int fd, int baud)
+{
+	struct termios tio = { };
+	tcflag_t baud_flag;
+
+	// see man tty_ioctl
+	tcgetattr(fd, &tio);
+
+	/* c_cflag serves to set both directions at once this way (CBAUD is a
+	 * mask for all the Bxxxx speed bits) (see termbits.h):
+	 *    (Bxxxx & CBAUD) << IBSHIFT for the input speed
+	 *    (Bxxxx & CBAUD) for the output speed.
+	 * A special baud rate value, BOTHER, indicates that the speed is to be
+	 * taken from the c_ispeed and c_ospeed fields instead.
+	 */
+	tio.c_cflag  = (tio.c_cflag & ~CSIZE) | CS8; // 8-bit
+	tio.c_cflag  = (tio.c_cflag & ~CSTOPB);      // 1-stop
+	tio.c_cflag &= ~(PARENB | PARODD);           // no parity
+	tio.c_cflag &= ~HUPCL;                       // no DTR/RTS down
+	tio.c_cflag &= ~CRTSCTS;                     // no flow control
+	tio.c_cflag |= CREAD;                        // enable rx
+	tio.c_cflag |= CLOCAL;                       // local echo
+
+	if (baud) {
+		//tio.c_cflag &= ~(CBAUD | (CBAUD << IBSHIFT));
+		//tio.c_cflag |= BOTHER | (BOTHER << IBSHIFT);
+		baud_flag = baud_encode(baud);
+		if (baud_flag == ~0) { /* baud rate not found */
+			errno = EINVAL;
+			return -1;
+		}
+		if (cfsetospeed(&tio, baud_flag) == -1)
+			return -1;
+		if (cfsetispeed(&tio, baud_flag) == -1)
+			return -1;
+	}
+	return tcsetattr(fd, TCSANOW, &tio);
+}
+#endif // TCSETS2 not defined
+
 int main(int argc, char **argv)
 {
 	const char *progname;
@@ -338,6 +578,7 @@ int main(int argc, char **argv)
 	int do_list = 0;
 	int do_wait_new = 0;
 	int do_print = 0;
+	int fd, ret;
 
 	progname = strrchr(argv[0], '/');
 	if (!progname)
@@ -508,26 +749,58 @@ int main(int argc, char **argv)
 	if (port && !parse_int(port, &currport))
 		currport = 0;
 
-	if (!port && nbports > 0)
-		port = serial_ports[currport].name;
-
-	if (strncmp(port, "/dev/", 5) == 0)
-		port += 5;
-
-	if (do_print) {
-		/* print the selected port and quit */
-		if (port) {
-			printf("%s\n", port);
-			return 0;
-		}
-		return 1;
-	}
-
 	if (do_list) {
 		/* list available ports and quit */
 		list_ports(-1);
 		return 0;
 	}
+
+	/* try to open the port */
+	do {
+		if (!port && nbports > 0) {
+			port = serial_ports[currport].name;
+			if (!do_print && !quiet && !do_wait_new) {
+				printf("No port specified, using %s (last registered). Use -l to list ports.\n", port);
+			}
+		}
+
+		if (do_print) {
+			/* print the selected port and quit */
+			if (port) {
+				printf("%s\n", port);
+				return 0;
+			}
+			return 1;
+		}
+
+		if (!quiet)
+			printf("Trying port %s...", port);
+
+		fd = open_port(port);
+
+		if (fd == -1) {
+			/* failures have different fallbacks:
+			 *  - EBUSY, ENODEV, ENOENT, ENOMEM will automatically
+			 *    be retried if the port was chosen automatically.
+			 *  - other errors always cause an abort
+			 */
+			if ((curr && *curr) && (currport < nbports) &&
+			    (errno == EBUSY || errno == ENODEV || errno == ENOENT || errno == ENOMEM)) {
+				currport++;
+				if (!quiet)
+					printf(" Failed! (%s)\n", strerror(errno));
+				continue;
+			}
+			die(2, "Failed to open port: %s\n", strerror(errno));
+		}
+	} while (fd == -1);
+
+	if (!quiet)
+		printf(" Connected.\n");
+
+	ret = set_port(fd, baud);
+	if (ret == -1)
+		die(3, "Failed to configure port: %s\n", strerror(errno));
 
 	return 0;
 }
